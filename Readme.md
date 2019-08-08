142f088f8f61c5191cf278bb32086876af61c2ae
Nico Audy <nicoaudy1994@gmail.com>
2019-08-08 11:37:30 +0700

    reorder cidro

--- Readme.md
+++ Readme.md
@@ -1,2 +1,5 @@
 # History
 When i'm working on React Native, to avoid uneccessary repetition i create this UI lib for basic components like Inputs, Buttons etc. By using this UI lib we can reduce the repition and get spicy ui 🌶️🌶️🔥🔥.
+
+## Requirements
+Cidro based on `styled-components` so you have to installed first.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Button from "./components/button";
-import Title from "./components/title";
-import Form from "./components/form/form";
-import FieldWrapper from "./components/form/field_wrapper";
-import TextInput from "./components/form/input";
+import Button from "./src/components/button";
+import Title from "./src/components/title";
+import Form from "./src/components/form/form";
+import FieldWrapper from "./src/components/form/field_wrapper";
+import TextInput from "./src/components/form/input";
 
 export { Button, Title, Form, FieldWrapper, TextInput };
