9866cddea57e2791465fe8a9a4092c3fa154bef4
Nico Audy <nicoaudy1994@gmail.com>
2019-08-16 14:47:23 +0700

    ++ add new components: avatar

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,36 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View, Image } from "react-native";
+import { Image } from "react-native";
 import { COLORS } from "../themes";
 
-const Avatar = ({ size, backgroundColor, source, circle, style }) => {
-  const containerStyle = {
+const Avatar = ({ size, source, circle, style }) => {
+  const imageStyle = {
     width: size,
-    height: size,
-    backgroundColor: backgroundColor,
-    alignItems: "center",
-    justifyContent: "center"
+    height: size
   };
 
-  if (circle) {
-    containerStyle.borderRadius = size / 2;
-    return (
-      <View style={[style, containerStyle]}>
-        <Image source={source} />
-      </View>
-    );
-  } else {
-    return (
-      <View style={[style, containerStyle]}>
-        <Image source={source} />
-      </View>
-    );
-  }
+  return (
+    <Image
+      style={[
+        style,
+        imageStyle,
+        circle && {
+          borderRadius: size / 2
+        }
+      ]}
+      source={source}
+    />
+  );
 };
 
 Avatar.propTypes = {
   size: PropTypes.number,
-  backgroundColor: PropTypes.string,
   source: PropTypes.any,
   circle: PropTypes.boolean,
   style: PropTypes.any
